2a5ff3b7444e35af3503497deb3c3ebbdd03f9ca
Denis Yordanov <deyordanov777@gmail.com>
2025-03-11 11:25:07 +0200

    Fixed an bug related to the exception type.

--- OJS.Workers.SubmissionProcessors/Models/ExceptionModel.cs
+++ OJS.Workers.SubmissionProcessors/Models/ExceptionModel.cs
@@ -1,13 +1,11 @@
 ﻿namespace OJS.Workers.SubmissionProcessors.Models
 {
-    using OJS.Workers.Common.Models;
-
     public class ExceptionModel
     {
         public string Message { get; set; }
 
         public string StackTrace { get; set; }
 
-        public ExceptionType? ExceptionType { get; set; }
+        public string ExceptionType { get; set; }
     }
 }
